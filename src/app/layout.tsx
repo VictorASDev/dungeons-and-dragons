0365ed0db8cc588153bb58ef78b7c8e079985eef
Victor <victor.augustosilva004@gmail.com>
2025-10-07 19:48:01 -0300

    fix: configuração da escala mínima do projeto
    adicionei um atributo meta ao head do projeto para impedir que usuários mobile possam dar zoom-out na página

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable}`}
       >
